d2ae5b4d2747a7a47c3c152941c5ee7afba88356
Hudson Treu <hudsontreu55@gmail.com>
2025-02-08 11:35:27 -0500

    Updated project page portable styling

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -28,7 +28,7 @@ export default function WorkPage() {
       <div className={styles.header}>
         <p className={styles.description}>
           Below are selected works including software design, software, development, and various forms of artworks.
-          Experiments are web based and run in real-time in the browser.
+          Experiments are web-based and run in real-time in the browser.
         </p>
       </div>
       <div className={styles.content}>
